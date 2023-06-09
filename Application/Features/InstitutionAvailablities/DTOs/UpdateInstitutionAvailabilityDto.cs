7f2bb0110839a902cbe390cf71ecb7240df4ffe3
sossyh <sosworkuacha@gmail.com>
2023-06-09 14:43:41 +0300

    Feat(backend-HakimHub): Add controller

--- Application/Features/InstitutionAvailablities/DTOs/UpdateInstitutionAvailabilityDto.cs
+++ Application/Features/InstitutionAvailablities/DTOs/UpdateInstitutionAvailabilityDto.cs
@@ -4,10 +4,6 @@ namespace Application.Features.InstitutionAvailabilities.DTOs
 {
     public class UpdateInstitutionAvailabilityDto : BaseDto, IInstitutionAvailabilityDto
     {
-        public DayOfWeek StartDay { get; set; }
-        public DayOfWeek EndDay { get; set; }
-        public string Opening { get; set; }
-        public string Closing { get; set; }
         public bool TwentyFourHours { get; set; }
         public DayOfWeek StartDay {get; set;}
 
