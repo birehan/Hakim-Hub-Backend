4dd023ffd5eb5dc2aef020466bbbd6541e126565
abrahamshimekt <abrahamshimektabrahamshimekt4096@gmail.com>
2023-06-11 22:39:10 +0300

    feat(Abraham.doctorprofile): add more fields

--- Domain/DoctorProfile.cs
+++ Domain/DoctorProfile.cs
@@ -1,37 +1,34 @@
 using System.Text.Json.Serialization;
 using Domain.Common;
-namespace Domain
+namespace Domain;
+
+
+
+public class DoctorProfile : BaseDomainEntity
 {
-    public class DoctorProfile : BaseDomainEntity
+    public enum GenderType
     {
-        public enum GenderType
-        {
-            Male,
-            Female
-        }
-
-        public string FullName { get; set; }
-        public string? About { get; set; }
-        public GenderType Gender { get; set; }
-        public string? Email { get; set; }
-        public string? PhotoId { get; set; }
-        public Photo? Photo { get; set; }
-        public DateTime CareerStartTime { get; set; }
-
-        public Guid? MainInstitutionId { get; set; }
-
-        [JsonIgnore]
-        public InstitutionProfile? MainInstitution { get; set; }
-        
-        [JsonIgnore]
-        public ICollection<InstitutionProfile> Institutions { get; set; } = new List<InstitutionProfile>();
-        public ICollection<DoctorAvailability> DoctorAvailabilities { get; set; } = new List<DoctorAvailability>();
-        public ICollection<Education> Educations { get; set; } = new List<Education>();
-        public ICollection<Experience> Experiences { get; set; } = new List<Experience>();
-
-        [JsonIgnore]
-        public ICollection<Speciality> Specialities { get; set; } = new List<Speciality>();
-
-       
+        Male,
+        Female
     }
+
+    public string FullName { get; set; }
+    public string About { get; set; }
+    public string Email { get; set; }
+    public string PhotoId { get; set; }
+    public Photo Photo { get; set; }
+    public DateTime CareerStartTime { get; set; }
+    public GenderType Gender { get; set; }
+    [JsonIgnore]
+    public InstitutionProfile? MainInstitution { get; set; }
+    [JsonIgnore]
+    public ICollection<InstitutionProfile> Institutions { get; set; } = new List<InstitutionProfile>();
+    public ICollection<DoctorAvailability> DoctorAvailabilities { get; set; } = new List<DoctorAvailability>();
+    public ICollection<Education> Educations { get; set; } = new List<Education>();
+    public ICollection<Experience> Experiences { get; set; } = new List<Experience>();
+    [JsonIgnore]
+    public ICollection<Speciality> Specialities { get; set; } = new List<Speciality>();
+
+
 }
+
