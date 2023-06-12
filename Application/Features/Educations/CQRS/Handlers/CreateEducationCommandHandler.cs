eae81a10927d586e5b05486e0f3af469c3b8b131
Tigist Wondimneh <wondimnehtigist@gmail.com>
2023-06-12 09:25:05 +0300

    feat(HakimHub-Backend): Refactor Dto attributes

--- Application/Features/Educations/CQRS/Handlers/CreateEducationCommandHandler.cs
+++ Application/Features/Educations/CQRS/Handlers/CreateEducationCommandHandler.cs
@@ -29,14 +29,12 @@ public class CreateEducationCommandHandler: IRequestHandler<CreateEducationComma
         if (!validationResult.IsValid)
             return Result<CreateEducationDto>.Failure(validationResult.Errors[0].ErrorMessage);
 
-
         var education = _mapper.Map<Education>(request.createEducationDto);
+        
         var edu = await _unitOfWork.EducationRepository.Add(education);
-
 
         if (await _unitOfWork.Save() > 0)
             return Result<CreateEducationDto>.Success(_mapper.Map<CreateEducationDto>(edu));
-
         return Result<CreateEducationDto>.Failure("Creation Failed");
     }
 
