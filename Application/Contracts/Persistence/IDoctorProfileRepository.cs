1b0389f2a6e6addd740e282b0a1eb398e07ee796
abrahamshimekt <abrahamshimektabrahamshimekt4096@gmail.com>
2023-06-14 09:29:19 +0300

    feat(Abraham.doctorprofile-crud):  define filters

--- Application/Contracts/Persistence/IDoctorProfileRepository.cs
+++ Application/Contracts/Persistence/IDoctorProfileRepository.cs
@@ -10,11 +10,13 @@ namespace Application.Contracts.Persistence
 {
     public interface IDoctorProfileRepository : IGenericRepository<DoctorProfile>
     {
+        public Task<DoctorProfile> GetDoctor(Guid Id);
         public Task<List<DoctorProfile>> GetDoctorProfileBySpecialityId(Guid SpecialityId);
         public Task<List<DoctorProfile>> GetDoctorProfileByInstitutionId(Guid InstitutionId);
         public Task<List<DoctorProfile>> GetDoctorProfileByGender(GenderType gender);
         public Task<List<DoctorProfile>> GetDoctorProfileByCareerStartTime(DateTime careerStartTime);
         public Task<List<DoctorProfile>> GetDoctorProfileBySpecialityIdAndGender(Guid specialityId, GenderType gender);
-        public Task<List<DoctorProfile>> FilterDoctors(Guid specialityID,Guid EducationId, DateTime careerStartTime,Guid institutionId);
+        public Task<List<DoctorProfile>> FilterDoctors(Guid institutionId, string? specialityName = null, DateTime? careerStartTime = null, string? educationInstitutionName = null);
+        
     }
 }
