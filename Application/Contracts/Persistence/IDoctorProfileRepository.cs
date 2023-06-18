ddff603551cee9cda54ca650c9eb0bddfd4b030f
abrahamshimekt <abrahamshimektabrahamshimekt4096@gmail.com>
2023-06-18 21:21:31 +0300

    feat(Abraham.doctorprofile-crud): remove unused Tasks

--- Application/Contracts/Persistence/IDoctorProfileRepository.cs
+++ Application/Contracts/Persistence/IDoctorProfileRepository.cs
@@ -10,13 +10,15 @@ namespace Application.Contracts.Persistence
 {
     public interface IDoctorProfileRepository : IGenericRepository<DoctorProfile>
     {
-        public Task<DoctorProfile> GetDoctor(Guid Id);
+        public Task<DoctorProfile> GetDoctorProfile(Guid Id);
+        public Task<DoctorProfile> GetDoctorProfileDetail(Guid Id);
         public Task<List<DoctorProfile>> GetDoctorProfileBySpecialityId(Guid SpecialityId);
+        public Task<List<DoctorProfile>> GetDoctorProfileByEducationId(Guid EducationId);
+
         public Task<List<DoctorProfile>> GetDoctorProfileByInstitutionId(Guid InstitutionId);
-        public Task<List<DoctorProfile>> GetDoctorProfileByGender(GenderType gender);
+
         public Task<List<DoctorProfile>> GetDoctorProfileByCareerStartTime(DateTime careerStartTime);
-        public Task<List<DoctorProfile>> GetDoctorProfileBySpecialityIdAndGender(Guid specialityId, GenderType gender);
-        public Task<List<DoctorProfile>> FilterDoctors(Guid institutionId, string? specialityName = null, DateTime? careerStartTime = null, string? educationInstitutionName = null);
+        public Task<List<DoctorProfile>> FilterDoctors(Guid? institutionId, string? specialityName = null, DateTime? careerStartTime = null, string? educationInstitutionName = null);
         
     }
 }
