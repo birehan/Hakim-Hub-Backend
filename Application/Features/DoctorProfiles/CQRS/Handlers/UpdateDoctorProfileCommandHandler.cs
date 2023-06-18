05fd862acfc5c570c6c24376ae6e9e17f406eb8d
abrahamshimekt <abrahamshimektabrahamshimekt4096@gmail.com>
2023-06-18 21:17:40 +0300

    feat(Abraham.doctorprofile-crud): add photoaccessor

--- Application/Features/DoctorProfiles/CQRS/Handlers/UpdateDoctorProfileCommandHandler.cs
+++ Application/Features/DoctorProfiles/CQRS/Handlers/UpdateDoctorProfileCommandHandler.cs
@@ -6,8 +6,10 @@ using Application.Contracts.Persistence;
 using Application.Exceptions;
 using Application.Features.DoctorProfiles.CQRS.Commands;
 using Application.Features.DoctorProfiles.DTOs.Validators;
+using Application.Interfaces;
 using Application.Responses;
 using AutoMapper;
+using Domain;
 using MediatR;
 
 namespace Application.Features.DoctorProfiles.CQRS.Handlers
@@ -16,52 +18,58 @@ namespace Application.Features.DoctorProfiles.CQRS.Handlers
     {
         private readonly IUnitOfWork _unitOfWork;
         private readonly IMapper _mapper;
-        public UpdateDoctorProfileCommandHandler(IUnitOfWork unitOfWork, IMapper mapper)
+        private readonly IPhotoAccessor _photoAccessor;
+
+        public UpdateDoctorProfileCommandHandler(IUnitOfWork unitOfWork, IMapper mapper, IPhotoAccessor photoAccessor)
         {
             _unitOfWork = unitOfWork;
             _mapper = mapper;
+            _photoAccessor = photoAccessor;
         }
+
         public async Task<Result<Unit>> Handle(UpdateDoctorProfileCommand request, CancellationToken cancellationToken)
         {
-            var response = new Result<Unit>();
-
             var validator = new UpdateDoctorProfileDtoValidator(_unitOfWork);
-
             var validationResult = await validator.ValidateAsync(request.updateDoctorProfileDto);
-            if (validationResult.IsValid == false)
-            {
-                response.IsSuccess = false;
-                response.Error = $"{validationResult.Errors.Select(e => e.ErrorMessage).ToList()}";
-                return response;
 
-            }
-            var doctorProfile = await _unitOfWork.DoctorProfileRepository.Get(request.updateDoctorProfileDto.Id);
-            if (doctorProfile is null)
+            if (!validationResult.IsValid)
             {
-                var error = new NotFoundException(nameof(doctorProfile), request.updateDoctorProfileDto.Id);
-                response.IsSuccess = false;
-                response.Error = $"{error}";
-                return response;
+                return Result<Unit>.Failure(string.Join(Environment.NewLine, validationResult.Errors.Select(e => e.ErrorMessage).ToList()));
             }
 
-            _mapper.Map(doctorProfile, request.updateDoctorProfileDto);
+            var doctorProfile = await _unitOfWork.DoctorProfileRepository.GetDoctorProfile(request.updateDoctorProfileDto.Id);
+            if (doctorProfile == null)
+            {
+                return Result<Unit>.Failure(new NotFoundException(nameof(doctorProfile), request.updateDoctorProfileDto.Id).Message);
+            }
+
+
+            _mapper.Map(request.updateDoctorProfileDto, doctorProfile);
+
+            if (request.updateDoctorProfileDto.DoctorPhoto != null)
+            {
+                var photoUploadResult = await _photoAccessor.AddPhoto(request.updateDoctorProfileDto.DoctorPhoto);
+                if (photoUploadResult == null)
+                {
+                    return Result<Unit>.Failure("Photo upload failed");
+                }
+
+                doctorProfile.Photo = new Photo
+                {
+                    Id = photoUploadResult.PublicId,
+                    Url = photoUploadResult.Url
+                };
+            }
+
             await _unitOfWork.DoctorProfileRepository.Update(doctorProfile);
-            if (await _unitOfWork.Save() == 0)
+
+            if (await _unitOfWork.Save() <= 0)
             {
-                response.IsSuccess = false;
-                response.Error = "server error";
-                return response;
-            }
-            else
-            {
-                response.IsSuccess = true;
-                return response;
+                return Result<Unit>.Failure("Server error");
             }
 
-
-
-
-
+            return Result<Unit>.Success(Unit.Value);
         }
     }
+
 }
