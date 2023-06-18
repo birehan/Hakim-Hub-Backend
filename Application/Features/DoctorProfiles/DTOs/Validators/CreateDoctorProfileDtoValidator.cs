070c74ee6ec5b03502da4e8dc779caa6cad40ba0
abrahamshimekt <abrahamshimektabrahamshimekt4096@gmail.com>
2023-06-18 21:15:57 +0300

    feat(Abraham.doctorprofile-crud): fix validator

--- Application/Features/DoctorProfiles/DTOs/Validators/CreateDoctorProfileDtoValidator.cs
+++ Application/Features/DoctorProfiles/DTOs/Validators/CreateDoctorProfileDtoValidator.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Domain;
 using FluentValidation;
+using Microsoft.AspNetCore.Http;
 using static Domain.DoctorProfile;
 
 namespace Application.Features.DoctorProfiles.DTOs.Validators
@@ -22,18 +23,10 @@ namespace Application.Features.DoctorProfiles.DTOs.Validators
             .WithMessage("{PropertyName} must be less than {PropertyValue}");
 
             RuleFor(p => p.About)
-           .NotNull()
-           .WithMessage("{PropertyName} is required")
-           .NotEmpty()
-           .WithMessage("{PropertyName} must be present")
            .MaximumLength(100)
            .WithMessage("{PropertyName} must be less than {PropertyValue}");
 
             RuleFor(p => p.Email)
-            .NotNull()
-            .WithMessage("{PropertyName} is required")
-            .NotEmpty()
-            .WithMessage("{PropertyName} must be present")
             .EmailAddress();
 
             RuleFor(p => p.CareerStartTime)
@@ -44,25 +37,17 @@ namespace Application.Features.DoctorProfiles.DTOs.Validators
             .YearMonthDate();
 
 
-            RuleFor(p => p.Photo)
-            .NotNull()
-            .WithMessage("{PropertyName} is required")
-            .NotEmpty()
-            .WithMessage("{PropertyName} must be present")
-            .IsValidPhotoExtension();
+            RuleFor(p => p.DoctorPhoto)
+                 .Must(CustomValidators.IsValidFileExtension)
+                 .WithMessage("{PropertyName} must have a valid file extension");
 
             RuleFor(p => p.Gender)
-            .NotNull()
-            .WithMessage("{PropertyName} is required")
-            .NotEmpty()
-            .WithMessage("{PropertyName} must be present")
-            .Must(gender => gender == GenderType.Male || gender == GenderType.Female)
-            .WithMessage("{PropertyName} must be 'male' or 'female'");
-
-
-
-
-
+                .NotNull()
+                .WithMessage("{PropertyName} is required")
+                .NotEmpty()
+                .WithMessage("{PropertyName} must be present")
+                .Must(gender => gender == GenderType.Male.ToString().ToLower() || gender == GenderType.Female.ToString().ToLower())
+                .WithMessage("{PropertyName} must be 'male' or 'female'");
         }
 
     }
