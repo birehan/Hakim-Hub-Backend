0e68a5f5ab54bf9607d5b883cc8e1a0ff89e0347
abrahamshimekt <abrahamshimektabrahamshimekt4096@gmail.com>
2023-06-18 21:22:38 +0300

    feat(Abraham.doctorprofile-crud): udate repository

--- Persistence/Repositories/DoctorProfileRepository.cs
+++ Persistence/Repositories/DoctorProfileRepository.cs
@@ -20,100 +20,100 @@ namespace Persistence.Repositories
             _dbContext = dbContext;
         }
 
-        public async Task<DoctorProfile> GetDoctor(Guid Id)
+        public async Task<DoctorProfile> GetDoctorProfile(Guid Id)
         {
-            var doctorProfile = await _dbContext.DoctorProfiles
-         .Where(d => d.Id == Id)
-         .Include(d=>d.Photo)
-         .Include(d=>d.Institutions)
-         .Include(d => d.Specialities)
-         .Include(d => d.Educations)
-         .Include(d=>d.Experiences)
-        
-         .FirstOrDefaultAsync();
+            var doctorProfile = await _dbContext.DoctorProfiles.Where(d=>d.Id==Id).FirstOrDefaultAsync();
             return doctorProfile;
         }
-        public async Task<List<DoctorProfile>> GetDoctorProfileBySpecialityId(Guid specialityId)
+
+        public async Task<DoctorProfile> GetDoctorProfileDetail(Guid Id)
         {
-            var doctorProfiles = await _dbContext.DoctorProfiles.Where(d => d.Specialities.Any(s => s.Id == specialityId)).Include(d=>d.Specialities)
-            .Include(d=>d.Photo)
+            var doctorProfile = await _dbContext.DoctorProfiles
+       .Where(d => d.Id == Id)
+       .Include(d => d.Photo)
+       .Include(d => d.MainInstitution)
+       .FirstOrDefaultAsync();
+            return doctorProfile;
+        }
+    
+    public async Task<List<DoctorProfile>> GetDoctorProfileBySpecialityId(Guid specialityId)
+    {
+        var doctorProfiles = await _dbContext.DoctorProfiles.Where(d => d.Specialities.Any(s => s.Id == specialityId)).Include(d => d.Specialities)
+        .Include(d => d.Photo)
+    .ToListAsync();
+        return doctorProfiles;
+    }
+
+    public async Task<List<DoctorProfile>> GetDoctorProfileByInstitutionId(Guid InstitutionId)
+    {
+        var doctorProfiles = await _dbContext.DoctorProfiles.Where(d => d.Institutions.Any(e => e.Id == InstitutionId))
+        .Include(d => d.Institutions)
+        .Include(d => d.Photo)
         .ToListAsync();
-            return doctorProfiles;
+        return doctorProfiles;
+    }
+
+    public async Task<List<DoctorProfile>> GetDoctorProfileByGender(GenderType gender)
+    {
+        var doctorProfiles = await _dbContext.DoctorProfiles.Where(d => d.Gender == gender)
+        .Include(d => d.Photo)
+        .ToListAsync();
+        return doctorProfiles;
+    }
+
+    public async Task<List<DoctorProfile>> GetDoctorProfileByCareerStartTime(DateTime careerStartTime)
+    {
+        var doctorProfiles = await _dbContext.DoctorProfiles.Where(d => d.CareerStartTime == careerStartTime)
+        .Include(d => d.Experiences)
+        .Include(d => d.Photo)
+        .ToListAsync();
+        return doctorProfiles;
+    }
+
+
+
+    public async Task<List<DoctorProfile>> FilterDoctors(Guid? institutionId, string? specialityName, DateTime? careerStartTime, string? educationInstitutionName)
+    {
+        var query = _dbContext.DoctorProfiles.AsQueryable();
+
+        // Filter by Institution ID
+        if (institutionId != Guid.Empty)
+        {
+            query = query.Where(d => d.Institutions.Any(i => i.Id == institutionId))
+            .Include(d => d.Specialities).Include(e => e.Educations);
         }
 
-        public async Task<List<DoctorProfile>> GetDoctorProfileByInstitutionId(Guid InstitutionId)
+        if (!string.IsNullOrEmpty(specialityName))
         {
-            var doctorProfiles = await _dbContext.DoctorProfiles.Where(d => d.Institutions.Any(e => e.Id == InstitutionId))
-            .Include(d=>d.Institutions)
-            .Include(d=>d.Photo)
+
+            query = query.Where(d => d.Specialities.Any(s => s.Name == specialityName));
+            Console.WriteLine(query);
+        }
+
+        if (!string.IsNullOrEmpty(educationInstitutionName))
+        {
+            query = query.Where(d => d.Educations.Any(e => e.EducationInstitution == educationInstitutionName));
+        }
+
+        if (careerStartTime.HasValue)
+        {
+            query = query.Where(d => d.CareerStartTime == careerStartTime);
+        }
+
+        var filteredDoctors = await query.ToListAsync();
+
+        return filteredDoctors;
+
+
+    }
+
+        public async Task<List<DoctorProfile>> GetDoctorProfileByEducationId(Guid EducationId)
+        {
+           var doctorProfiles = await _dbContext.DoctorProfiles.Where(d => d.Educations.Any(e => e.Id == EducationId))
+            .Include(d => d.Educations)
+            .Include(d => d.Photo)
             .ToListAsync();
             return doctorProfiles;
         }
-
-        public async Task<List<DoctorProfile>> GetDoctorProfileByGender(GenderType gender)
-        {
-            var doctorProfiles = await _dbContext.DoctorProfiles.Where(d => d.Gender == gender)
-            .Include(d=>d.Photo)
-            .ToListAsync();
-            return doctorProfiles;
-        }
-
-        public async Task<List<DoctorProfile>> GetDoctorProfileByCareerStartTime(DateTime careerStartTime)
-        {
-            var doctorProfiles = await _dbContext.DoctorProfiles.Where(d => d.CareerStartTime == careerStartTime)
-            .Include(d=>d.Experiences)
-            .Include(d=>d.Photo)
-            .ToListAsync();
-            return doctorProfiles;
-        }
-
-        public async Task<List<DoctorProfile>> GetDoctorProfileBySpecialityIdAndGender(Guid specialityId, GenderType gender)
-        {
-            var doctorProfiles = await _dbContext.DoctorProfiles.Where(d => d.Specialities.Any(s => s.Id == specialityId) && d.Gender == gender)
-            .Include(d=>d.Specialities)
-            .Include(d=>d.Photo)
-            .ToListAsync();
-            return doctorProfiles;
-        }
-
-
-
-        public async Task<List<DoctorProfile>> FilterDoctors(Guid institutionId, string? specialityName, DateTime? careerStartTime, string? educationInstitutionName)
-        {
-            var query = _dbContext.DoctorProfiles.AsQueryable();
-            query = query.Where(d=>d.Institutions.Any(i=>i.Id==institutionId));
-            
-            // Filter by Institution ID
-            if (institutionId != Guid.Empty)
-            {
-                query = query.Where(d => d.Institutions.Any(i => i.Id == institutionId))
-                .Include(d => d.Specialities).Include(e => e.Educations);
-            }
-
-            if (!string.IsNullOrEmpty(specialityName))
-            {
-
-                query = query.Where(d => d.Specialities.Any(s => s.Name == specialityName));
-                Console.WriteLine(query);
-            }
-
-            if (!string.IsNullOrEmpty(educationInstitutionName))
-            {
-                query = query.Where(d => d.Educations.Any(e => e.EducationInstitution == educationInstitutionName));
-            }
-
-            if (careerStartTime.HasValue)
-            {
-                query = query.Where(d => d.CareerStartTime == careerStartTime);
-            }
-
-            var filteredDoctors = await query.ToListAsync();
-
-            return filteredDoctors;
-
-
-        }
-
-
     }
 }
