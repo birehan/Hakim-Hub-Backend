1823a463381a6316ac83f66670ff06360d8282c5
YabTek <yabtekuam@gmail.comremote: Invalid username or password>
2023-06-19 20:52:14 +0300

    feat(Yeabsira.chatbot) add models & dtos

--- Application/Features/Chat/Models/Data.cs
+++ Application/Features/Chat/Models/Data.cs
@@ -1,6 +1,7 @@
+namespace Application.Features.Chat.Models;
 
 public class Data
 {
-    public string? message { get; set; }
-    public List<string> specializations { get; set; } = new List<string>();
+    public string? Message { get; set; }
+    public List<string>? Specializations { get; set; }
 }
