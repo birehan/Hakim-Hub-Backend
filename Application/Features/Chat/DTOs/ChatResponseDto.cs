9915eead56b206f00db2764141d4b2eb4dea8932
YabTek <yabtekuam@gmail.comremote: Invalid username or password>
2023-06-20 13:20:55 +0300

    feat(Yeabsira.chatbot)modify files

--- Application/Features/Chat/DTOs/ChatResponseDto.cs
+++ Application/Features/Chat/DTOs/ChatResponseDto.cs
@@ -1,12 +1,15 @@
 using Application.Features.Chat.Models;
+using Application.Features.DoctorProfiles.DTOs;
+using Application.Features.InstitutionProfiles.DTOs;
 using Domain;
 
 namespace Application.Features.Chat.DTOs;
 
 public class ChatResponseDto
 {
-    public string? replyMessage { get; set; }
-    public Error? ErrorMessage { get; set; }
-    public List<DoctorProfile>? Doctors { get; set; }
-    public List<InstitutionProfile>? Institutions { get; set; }
+    public string reply {get; set;}
+
+    public List<DoctorProfileDetailDto>? Doctors {get; set;}
+    
+    public List<InstitutionProfileDetailDto>? Institutions {get; set;}
 }
