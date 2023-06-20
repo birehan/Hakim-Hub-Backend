7787bdfae63d6dca2ebcb82f500681c7881666e1
YabTek <yabtekuam@gmail.comremote: Invalid username or password>
2023-06-20 10:01:02 +0300

    feat(Yeabsira.chatbot) add controller&query

--- Application/Features/Chat/CQRS/Queries/ChatRequestQuery.cs
+++ Application/Features/Chat/CQRS/Queries/ChatRequestQuery.cs
@@ -5,8 +5,8 @@ namespace Application.Features.Chat.CQRS.Queries
 {
     public class ChatRequestQuery : IRequest<ChatRequestDto>
     {
-        public string Message { get; set; }
-        public string IpAddress { get; set; }
+        public string? Message { get; set; }
+        public string? IpAddress { get; set; }
         public bool IsNewChat { get; set; }
     }
 }
