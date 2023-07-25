68b14f347116fe5b42c6f9acb84f1231414cf68a
abrahamshimekt <abrahamshimektabrahamshimekt4096@gmail.com>
2023-07-25 07:38:50 +0300

    feat(Abraham.doctorProfile-crud): remove function create command

--- API/Program.cs
+++ API/Program.cs
@@ -17,8 +17,6 @@ builder.Services.ConfigurePersistenceServices(builder.Configuration);
 builder.Services.ConfigureApplicationServices();
 builder.Services.ConfigureInfrastructureServices();
 builder.Services.AddIdentityServices(builder.Configuration);
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-
 
 var app = builder.Build();
 
@@ -48,45 +46,12 @@ using var scope = app.Services.CreateScope();
 var services = scope.ServiceProvider;
 
 
-{
-    using (var connection = new NpgsqlConnection(connectionString))
-    {
-        connection.Open();
-
-        using (var command = new NpgsqlCommand())
-        {
-            command.Connection = connection;
-
-            command.CommandText = @"
-                CREATE OR REPLACE FUNCTION calculate_distance(
-                    lat1 double precision,
-                    lon1 double precision,
-                    lat2 double precision,
-                    lon2 double precision
-                )
-                RETURNS double precision AS $$
-                DECLARE
-                    x double precision = 69.1 * (lat2 - lat1);
-                    y double precision = 69.1 * (lon2 - lon1) * cos(lat1 / 57.3);
-                    distance double precision = sqrt(x * x + y * y);
-                BEGIN
-                    RETURN distance;
-                END;
-                $$ LANGUAGE plpgsql;
-            ";
-
-            command.ExecuteNonQuery();
-        }
-    }
-}
-
 try
 {
     var context = services.GetRequiredService<HakimHubDbContext>();
     var userManager = services.GetRequiredService<UserManager<AppUser>>();
 
     await context.Database.MigrateAsync();
-    // await Seed.SeedData(context, );
     await Seed.SeedData(context, userManager, builder.Configuration);
 
 }
